ee98b916719ac73e377e2894d64e2ad4da3dd52f
Angel Ernesto Aguirre Prieto <65724204+aeaguirre2@users.noreply.github.com>
2023-05-30 19:19:06 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,58 +5,17 @@ gsap.to(".container1", {
 })
 gsap.to(".container2", {
   x:800,
-  duration: 4,
+  duration: 0,
+  opacity:0,
 })
-gsap.fromTo("nav", {
-  opacity: 0,
-}, {
-  opacity: 1,
-  duration: 2,
-  delay: 2,
-});
+
 gsap.fromTo("#video-container", {
   opacity: 0,
 }, {
   opacity: 1,
   duration: 2,
 });
-gsap.to('#my-text', {
-  y: 0,
-  duration: .3,
-  delay: 2.3,
-})
-gsap.to('#my-text2', {
-  y: 0,
-  duration: .3,
-  delay: 2.3,
-})
-gsap.fromTo(".img-r2", {
-  opacity: 0,
-  y: 100,
-}, {
-  y: 0,
-  opacity: 1,
-  duration: 2,
-  delay: 3.5,
-})
-gsap.fromTo(".img-r3", {
-  opacity: 0,
-  x:-300,
-}, {
-  x:0,
-  opacity: 1,
-  duration: 2,
-  delay: 3,
-})
-gsap.fromTo(".img-r4", {
-  opacity: 0,
-  y:-100
-}, {
-  y:0,
-  opacity: 1,
-  duration: 3,
-  delay: 2.5,
-})
+
 
 gsap.to("#move", {
   scrollTrigger: {
